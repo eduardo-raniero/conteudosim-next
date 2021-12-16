fa9fa2b1cefc8696a025927a2e73dc12fdb4b38b
Eduardo_Raniero <eduardo.silva@breakout.com.br>
2021-12-16 14:16:08 -0200

    auto-focus-false

--- pages/index.jsx
+++ pages/index.jsx
@@ -214,7 +214,6 @@ export default function Home() {
                 onChange={handleChange}
                 onBlur={handleChange}
                 color="secondary"
-                autoFocus={true}
                 fullWidth 
               />
               {errors.Nome && <small> {errors.Nome} </small>}
